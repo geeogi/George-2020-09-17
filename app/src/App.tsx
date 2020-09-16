2ed673d67e05cec62540484c92dffd0a394db9f8
George Bennett <george6bennett@gmail.com>
2020-09-16 10:27:33 +0100

    add useResources hook

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -4,20 +4,17 @@ import { ResourcePreview } from "./components/ResourcePreview";
 import { ResourcesContainer } from "./components/ResourcesContainer";
 import { ResourcesOverview } from "./components/ResourcesOverview";
 import { bytesToKilobytes, round } from "./core/util";
-import { Resource } from "./model/resource";
-
-const MOCK_RESOURCES: Resource[] = [
-  { id: "1", name: "My super picture", size: 30000 },
-  { id: "2", name: "My very good picture", size: 15000 },
-  { id: "3", name: "My picture", size: 8000 },
-  { id: "4", name: "My picture with a long name", size: 90000 },
-  { id: "5", name: "My brilliant picture", size: 25000 },
-  { id: "6", name: "My fantastic picture", size: 7000 },
-];
+import { useResources } from "./hooks/resources";
 
 function App() {
-  const numberOfDocuments = MOCK_RESOURCES.length;
-  const totalSizeB = MOCK_RESOURCES.reduce((a, b) => a + b.size, 0);
+  const { resources } = useResources();
+
+  if (!resources) {
+    return <div>Loading...</div>;
+  }
+
+  const numberOfDocuments = resources.length;
+  const totalSizeB = resources.reduce((a, b) => a + b.size, 0);
   const totalSizeKb = round(bytesToKilobytes(totalSizeB));
 
   return (
@@ -25,7 +22,7 @@ function App() {
       <ControlsBar />
       <ResourcesOverview numberOfDocuments={numberOfDocuments} totalSizeKb={totalSizeKb} />
       <ResourcesContainer>
-        {MOCK_RESOURCES.map((resource) => (
+        {resources.map((resource) => (
           <ResourcePreview resource={resource} />
         ))}
       </ResourcesContainer>
---
+++ app/src/hooks/resources.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { deleteResourceById, fetchResources, searchResources } from "../handlers/resource";
+import { Resource } from "../model/resource";
+
+export const useResources = () => {
+  const [resources, setResources] = useState<Resource[]>();
+  const [searchTerm, setSearchTerm] = useState<string>();
+
+  useEffect(() => {
+    if (!resources || !searchTerm) {
+      fetchResources().then(setResources);
+    }
+  });
+
+  useEffect(() => {
+    if (searchTerm) {
+      searchResources(searchTerm).then(setResources);
+    }
+  }, [searchTerm]);
+
+  const deleteResource = async (resource: Resource) => {
+    await deleteResourceById(resource.id);
+    const updatedResourceList = await fetchResources();
+    setResources(updatedResourceList);
+  };
+
+  return { resources, deleteResource, setSearchTerm };
+};
