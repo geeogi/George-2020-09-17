96250021e3a651f096761b10bf1cf34b03dc8057
George Bennett <george6bennett@gmail.com>
2020-09-16 09:22:03 +0100

    add HTML template

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,46 @@
 import React from "react";
 import "./App.css";
-import { PERMISSABLE_FILE_TYPES_MIME } from "./config/constants";
-import { createResourceFromFile, deleteResourceById, fetchResources, searchResources } from "./handlers/resource";
+import { bytesToKilobytes, round } from "./core/util";
+import { Resource } from "./model/resource";
+
+const MOCK_RESOURCES: Resource[] = [
+  { id: "1", name: "My super picture", size: 30000 },
+  { id: "2", name: "My very good picture", size: 15000 },
+  { id: "3", name: "My picture", size: 8000 },
+  { id: "4", name: "My picture with a long name", size: 90000 },
+  { id: "5", name: "My brilliant picture", size: 25000 },
+  { id: "6", name: "My fantastic picture", size: 7000 },
+];
 
 function App() {
+  const numberOfDocuments = MOCK_RESOURCES.length;
+  const totalSizeB = MOCK_RESOURCES.reduce((a, b) => a + b.size, 0);
+  const totalSizeKb = round(bytesToKilobytes(totalSizeB));
+
   return (
-    <div className="App">
-      <input
-        type="file"
-        accept={PERMISSABLE_FILE_TYPES_MIME.join(",")}
-        onChange={(e) => {
-          if (e.target.files && e.target.files.length === 1) {
-            createResourceFromFile(e.target.files[0]);
-          }
-        }}
-      ></input>
-      <button onClick={fetchResources}>Fetch</button>
-      <button onClick={() => searchResources("red")}>Search</button>
-      <button onClick={() => deleteResourceById("aoi1-39e03-d0sjs-dos")}>Delete</button>
-    </div>
+    <main className="container">
+      <div className="flex wrap-reverse space-between">
+        <input placeholder="Search documents..."></input>
+        <button>UPLOAD</button>
+      </div>
+      <div className="flex wrap-reverse space-between">
+        <h2>{numberOfDocuments} documents</h2>
+        <p>Total size: {totalSizeKb}kb</p>
+      </div>
+      <div className="flex wrap space-between">
+        {MOCK_RESOURCES.map((resource) => (
+          <div className="border my-8 document-width">
+            <div className="pa-16">
+              <h3>{resource.name}</h3>
+              <div className="flex space-between center">
+                <p>{round(bytesToKilobytes(resource.size))}kb</p>
+                <button>delete</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
 
--- app/src/core/util.ts
+++ app/src/core/util.ts
@@ -1,3 +1,9 @@
 export const bytesToMegabytes = (bytes: number) => {
   return bytes / 1024 / 1024;
 };
+
+export const bytesToKilobytes = (bytes: number) => {
+  return bytes / 1024;
+};
+
+export const round = (num: number) => Math.round(num);
